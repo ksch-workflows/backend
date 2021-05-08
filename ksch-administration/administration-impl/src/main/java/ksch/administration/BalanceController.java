0a4c99b2fa15e6a070a61094df855ff70f15bd5a
Jan Mewes <jan.mewes@ks-plus.org>
2021-05-08 17:24:33 +0200

    WIP

--- ksch-administration/administration-impl/src/main/java/ksch/administration/BalanceController.java
+++ ksch-administration/administration-impl/src/main/java/ksch/administration/BalanceController.java
@@ -1,8 +1,13 @@
 package ksch.administration;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BalanceController {
 
+    @GetMapping("/balance")
+    public String getBalance() {
+        return "balance";
+    }
 }
