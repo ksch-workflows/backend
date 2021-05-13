559c74a0e2b1f5696b7fd8cf1625d5deb784f3d9
Jan Mewes <jan.mewes@ks-plus.org>
2021-05-13 16:06:07 +0200

    Add CI workflow

---
+++ ksch-patient-management/patient-management-impl/src/test/java/ksch/patientmanagement/ExampleTest.java
@@ -0,0 +1,12 @@
+package ksch.patientmanagement;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void should_pass() {
+        Assertions.assertTrue(true);
+    }
+}
---
+++ ksch-reporting/reporting-impl/src/test/java/ksch/reporting/ExampleTest.java
@@ -0,0 +1,12 @@
+package ksch.reporting;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void should_pass() {
+        Assertions.assertTrue(true);
+    }
+}
