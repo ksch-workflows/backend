b0881ef1343005fa77ad33234134c6c77e9bf59b
Jan Mewes <jan.mewes@ks-plus.org>
2021-05-22 18:10:50 +0200

    Simplify code

--- commons/test/src/main/java/ksch/TestApplication.java
+++ commons/test/src/main/java/ksch/TestApplication.java
@@ -1,12 +1,6 @@
 package ksch;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan("ksch")
-@EntityScan("ksch")
-@EnableJpaRepositories("ksch")
 public class TestApplication {}
