a77efe9d1ff0ab24a9bd06874e61c41ac8d477e4
Jan Mewes <jan.mewes@ks-plus.org>
2021-05-22 19:09:43 +0200

    Remove obsolete path [skip ci]

--- docs/api/build.sh
+++ docs/api/build.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $SCRIPT_DIR
 
-asciidoctor --base-dir ../../build/generated-snippets  --out-file ../../docs/index.html index.adoc
+asciidoctor --out-file ../../docs/index.html index.adoc
