03c4618bfb0d686773d80e3330291b9f18399521
Jan Mewes <jan.mewes@experimental-software.com>
2021-07-30 08:33:55 +0200

    Add test for OPD number generator

--- ksch-visit/visit-impl/src/main/java/ksch/visit/infrastructure/VisitJpaRepository.java
+++ ksch-visit/visit-impl/src/main/java/ksch/visit/infrastructure/VisitJpaRepository.java
@@ -22,8 +22,8 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
-interface VisitJpaRepository extends JpaRepository<VisitDao, UUID>, JpaSpecificationExecutor<VisitDao>
-{
+interface VisitJpaRepository extends JpaRepository<VisitDao, UUID>, JpaSpecificationExecutor<VisitDao> {
+
     @Query("select count(v) > 0 from VisitDao v where v.patientId = :patientId and v.timeEnd is null")
     boolean hasActiveVisit(@Param("patientId") UUID patientId);
 }
---
+++ ksch-visit/visit-impl/src/test/java/ksch/visit/domain/OpdNumberGeneratorTest.java
@@ -0,0 +1,51 @@
+package ksch.visit.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Calendar;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+class OpdNumberGeneratorTest {
+
+    private static final int NUMERIC_VALUE = 1011;
+
+    @InjectMocks
+    private OpdNumberGenerator opdNumberGenerator;
+
+    @Mock
+    private OpdNumberRepository opdNumberRepository;
+
+    private final int currentYearWithTwoDigits = Calendar.getInstance().get(Calendar.YEAR) % 100;
+
+    @BeforeEach
+    public void setup() {
+        when(opdNumberRepository.save(any(NumericValue.class)))
+                .thenReturn(new NumericValue(NUMERIC_VALUE));
+    }
+
+    @Test
+    public void should_prepend_year() {
+        String generatedOpdNumber = opdNumberGenerator.generateOpdNumber();
+
+        assertTrue("Generated OPD number '" + generatedOpdNumber + "' doesn't start with current year",
+                generatedOpdNumber.startsWith(currentYearWithTwoDigits + "-"));
+    }
+
+    @Test
+    public void should_contain_patient_index_number() {
+        String generatedPatientNumber = opdNumberGenerator.generateOpdNumber();
+
+        assertTrue("Generated patient number '" + generatedPatientNumber +
+                        "' doesn't contain the patient index number '" + NUMERIC_VALUE + "'.",
+                generatedPatientNumber.contains("-" + NUMERIC_VALUE));
+    }
+}
