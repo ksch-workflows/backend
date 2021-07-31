448464bb6e7d22eb159f1751d3e7e68f85096351
Jan Mewes <jan.mewes@experimental-software.com>
2021-07-31 11:38:28 +0200

    Housekeeping

--- ksch-visit/visit-impl/src/main/java/ksch/visit/infrastructure/VisitJpaRepository.java
+++ ksch-visit/visit-impl/src/main/java/ksch/visit/infrastructure/VisitJpaRepository.java
@@ -16,13 +16,12 @@
 package ksch.visit.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
-interface VisitJpaRepository extends JpaRepository<VisitDao, UUID>, JpaSpecificationExecutor<VisitDao> {
+interface VisitJpaRepository extends JpaRepository<VisitDao, UUID> {
 
     @Query("select count(v) > 0 from VisitDao v where v.patientId = :patientId and v.timeEnd is null")
     boolean hasActiveVisit(@Param("patientId") UUID patientId);
