d571c880dc763ea960ca7f191bd6f7c3663c5c18
Jan Mewes <jan.mewes@ks-plus.org>
2021-12-06 11:17:02 +0100

    Add Spring Boot actuator

--- server/src/main/java/ksch/BackendApplication.java
+++ server/src/main/java/ksch/BackendApplication.java
@@ -16,6 +16,7 @@
 package ksch;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
