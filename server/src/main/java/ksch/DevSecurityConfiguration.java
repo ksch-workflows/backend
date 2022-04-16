3ec0cf3c823a5ac7c494ba0f2757567c1a24a529
Jan Mewes <jan.mewes@ks-plus.org>
2022-04-16 10:44:25 +0200

    Allow access on BFF

--- server/src/main/java/ksch/DevSecurityConfiguration.java
+++ server/src/main/java/ksch/DevSecurityConfiguration.java
@@ -13,6 +13,8 @@ public class DevSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers("/bff/**").permitAll()
             .antMatchers("/h2-console/**").permitAll()
             .anyRequest()
             .authenticated()
