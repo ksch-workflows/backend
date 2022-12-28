9fe25039a5633f8bdf2a83ad79e6ef1a082d996d
Jan Mewes <jan.mewes@ks-plus.org>
2022-12-28 15:07:11 +0100

    fix: enable cors preflight check in dev mode

--- server/src/main/java/ksch/DevSecurityConfiguration.java
+++ server/src/main/java/ksch/DevSecurityConfiguration.java
@@ -23,20 +23,37 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter;
 
+import static org.springframework.http.HttpMethod.OPTIONS;
+
+/**
+ * <h3>References</h3>
+ * <ul>
+ *     <li>
+ *         <a href="https://www.baeldung.com/spring-security-whitelist-ip-range">
+ *             Spring Security – Whitelist IP Range | baeldung.com
+*          </a>
+ *     </li>
+ * </ul>
+ */
 @Configuration
 @Profile("dev")
 @RequiredArgsConstructor
 @SuppressWarnings("squid:S4502") // CSRF handling will be tackled with https://github.com/ksch-workflows/backend/issues/51
 public class DevSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private static final String LOCALHOST = "127.0.0.1";
+
     private final TokenFilter tokenFilter;
 
+    @SuppressWarnings("squid:S4601") // IP address check has to be done first
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
+        http.cors().disable();
         http.addFilterBefore(tokenFilter, BearerTokenAuthenticationFilter.class);
         http
             .authorizeRequests()
+            .antMatchers("**").hasIpAddress(LOCALHOST)
+            .antMatchers(OPTIONS, "/api/**").permitAll()
             .antMatchers("/").permitAll()
             .antMatchers("/registration-desk/**").permitAll()
             .antMatchers("/bff/**").permitAll()
