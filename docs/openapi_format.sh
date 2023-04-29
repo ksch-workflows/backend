49991921648edb51f631150288c10963206e2f22
Jan Mewes <jan.mewes@ks-plus.org>
2023-04-29 16:59:47 +0200

    WIP

--- docs/openapi_format.sh
+++ docs/openapi_format.sh
@@ -2,4 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-npx openapi-format ${SCRIPT_DIR}/openapi.yml --output ${SCRIPT_DIR}/openapi.yml --sortComponentsFile ${SCRIPT_DIR}/sortComponents.json
+npx openapi-format ${SCRIPT_DIR}/openapi.yml --output ${SCRIPT_DIR}/openapi.yml --sortComponentsFile ${SCRIPT_DIR}/sortComponents.json --casingFile ${SCRIPT_DIR}/customCasing.yml
+
+# --casingFile ${SCRIPT_DIR}/casing.json
+# --configFile ${SCRIPT_DIR}/config.json
