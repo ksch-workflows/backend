de862cd967d17e51a30abde785cbefa1e963724a
Jan Mewes <jan.mewes@ks-plus.org>
2024-04-13 06:49:10 +0200

    Add test for patient entity factory method

---
+++ ksch.patientmanagement/ksch.patientmanagement.core/src/test/java/ksch/patientmanagement/core/PatientEntityTest.java
@@ -0,0 +1,20 @@
+package ksch.patientmanagement.core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.instancio.Instancio;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PatientEntityTest {
+
+    @Test
+    @DisplayName("Should create patient entity from data interface")
+    void test_from_factory_happy_path() {
+        var originalPatientEntity = Instancio.create(PatientEntity.class);
+
+        var clonedPatientEntity = PatientEntity.from(originalPatientEntity);
+
+        assertThat(clonedPatientEntity).isEqualTo(originalPatientEntity);
+    }
+}
